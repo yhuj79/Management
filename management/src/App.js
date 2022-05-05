02f89941323dbeab1e1a90ca445de6284dc7253b
yhuj79 <yu1085@naver.com>
2022-05-05 21:38:49 +0900

    22.05.05.commit.

--- management/src/App.js
+++ management/src/App.js
@@ -1,25 +1,48 @@
 import Customer from "./components/Customer";
 
-const customer = {
-  id: 1,
-  image: "https://placeimg.com/64/64/any",
-  name: "홍길동",
-  birthday: "980709",
-  gender: "남자",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "980709",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "호날두",
+    birthday: "850205",
+    gender: "남자",
+    job: "축구선수",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "메시",
+    birthday: "870624",
+    gender: "남자",
+    job: "축구선수",
+  },
+];
 
 function App() {
   return (
     <div>
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      {customers.map((m) => {
+        return (
+          <Customer
+            key={m.id}
+            id={m.id}
+            image={m.image}
+            name={m.name}
+            birthday={m.birthday}
+            gender={m.gender}
+            job={m.job}
+          />
+        );
+      })}
     </div>
   );
 }
