2b766a19636aa27e3fc43a6912b2a28389753d40
yhuj79 <yu1085@naver.com>
2022-05-13 11:52:25 +0900

    table

--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -1,31 +1,22 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-function Customer({id, image, name, birthday, gender, job}) {
-    return (
-        <div>
-            <CustomerProfile image={image} name={name} id={id} />
-            <CustomerInfo birthday={birthday} gender={gender} job={job} />
-        </div>
-    );
+function Customer({ id, image, name, birthday, gender, job }) {
+  return (
+    <CustomerDiv>
+      <div>{id}</div>
+      <div>
+        <img src={image} alt="profile" />
+      </div>
+      <div>{name}</div>
+      <div>{birthday}</div>
+      <div>{gender}</div>
+      <div>{job}</div>
+    </CustomerDiv>
+  );
 }
-
-function CustomerProfile({image, name, id}) {
-    return (
-        <div>
-            <img src={image} alt="profile" />
-            <h2>{name}({id})</h2>
-        </div>
-    );
-}
-
-function CustomerInfo({birthday, gender, job}) {
-    return (
-        <div>
-            <p>{birthday}</p>
-            <p>{gender}</p>
-            <p>{job}</p>
-        </div>
-    );
-}
-
+const CustomerDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 export default Customer;
