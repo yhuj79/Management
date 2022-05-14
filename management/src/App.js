69e5038b71f6c24d98d6fcb422e66f020c52a335
yhuj79 <yu1085@naver.com>
2022-05-14 12:11:09 +0900

    Table2

--- management/src/App.js
+++ management/src/App.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Customer from "./components/Customer";
 
 const customers = [
@@ -30,21 +31,40 @@ const customers = [
 function App() {
   return (
     <div>
-      {customers.map((m) => {
-        return (
-          <Customer
-            key={m.id}
-            id={m.id}
-            image={m.image}
-            name={m.name}
-            birthday={m.birthday}
-            gender={m.gender}
-            job={m.job}
-          />
-        );
-      })}
+      <HeadDiv>
+        <div className="head-id">번호</div>
+        <div className="head-image">프로필 이미지</div>
+        <div className="head-name">이름</div>
+        <div className="head-birthday">생년월일</div>
+        <div className="head-gender">성별</div>
+        <div className="head-job">직업</div>
+      </HeadDiv>
+      <div>
+        {customers.map((m) => {
+          return (
+            <Customer
+              key={m.id}
+              id={m.id}
+              image={m.image}
+              name={m.name}
+              birthday={m.birthday}
+              gender={m.gender}
+              job={m.job}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
+const HeadDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 20px;
 
+  .head-id, .head-image, .head-name,
+  .head-birthday, .head-gender, .head-job {
+    width: 200px;
+  }
+`;
 export default App;
--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -4,19 +4,30 @@ import styled from "styled-components";
 function Customer({ id, image, name, birthday, gender, job }) {
   return (
     <CustomerDiv>
-      <div>{id}</div>
-      <div>
+      <div className="customer-id">{id}</div>
+      <div className="customer-image">
         <img src={image} alt="profile" />
       </div>
-      <div>{name}</div>
-      <div>{birthday}</div>
-      <div>{gender}</div>
-      <div>{job}</div>
+      <div className="customer-name">{name}</div>
+      <div className="customer-birthday">{birthday}</div>
+      <div className="customer-gender">{gender}</div>
+      <div className="customer-job">{job}</div>
     </CustomerDiv>
   );
 }
 const CustomerDiv = styled.div`
   display: flex;
-  justify-content: space-between;
-`;
+  justify-content: center;
+
+  .customer-id, .customer-image, .customer-name,
+  .customer-birthday, .customer-gender, .customer-job {
+    width: 200px;
+    height: 80px;
+    line-height: 6;
+  }
+
+  .customer-image {
+    margin-top: 18px;
+  }
+`
 export default Customer;
